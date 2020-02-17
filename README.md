bb596f9f1bd19b3305ee71b832a3f083a3ef33ee
Alexander Korolev <kilork@yandex.ru>
2020-02-18 00:12:00 +0100

    reserve placeholder

---
+++ README.md
@@ -0,0 +1,16 @@
+# rsbt description
+
+## Legal
+
+Dual-licensed under `MIT` or the [UNLICENSE](http://unlicense.org/).
+
+## Features
+
+## Usage
+
+Add dependency to Cargo.toml:
+
+```toml
+[dependencies]
+rsbt = "0.1"
+```
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+/*!
+# rsbt description
+
+## Features
+
+## Usage
+
+Add dependency to Cargo.toml:
+
+```toml
+[dependencies]
+rsbt = "0.1"
+```
+
+*/
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
