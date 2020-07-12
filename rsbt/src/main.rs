c68de7c64e4087927b6055cf71737be236473140
Alexander Korolev <kilork@yandex.ru>
2020-07-12 22:49:52 +0200

    play with bootstrap 4.5 icons

--- rsbt/src/main.rs
+++ rsbt/src/main.rs
@@ -1,28 +1,31 @@
 /*!
-# rust description
+# rsbt description
 
 ## Features
 
 ## Usage
 
-Add dependency to Cargo.toml:
-
-```toml
-[dependencies]
-rust = "0.1"
-```
-
 */
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, http::StatusCode, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn index() -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
+    eprintln!("{:?}", std::env::current_dir());
+    if let Ok(index_html) = std::fs::read_to_string("rsbt/web/index.html") {
+        HttpResponse::build(StatusCode::OK)
+            .header("Content-Type", "text/html")
+            .body(index_html)
+    } else {
+        HttpResponse::NotFound().body("not found")
+    }
+    // .body(&include_bytes!("../web/index.html")[..])
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
     HttpServer::new(|| App::new().service(index))
         .bind("127.0.0.1:8080")?
         .run()
