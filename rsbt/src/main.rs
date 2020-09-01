60057a31814a71ee1c399be1fd691f5e32bf16c3
Alexander Korolev <kilork@yandex.ru>
2020-09-01 21:38:39 +0200

    implement quit from wizard

--- rsbt/src/main.rs
+++ rsbt/src/main.rs
@@ -7,14 +7,32 @@
 
 */
 
-use actix_web::{get, http::StatusCode, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    dev::Server, get, http::StatusCode, post, web, App, HttpResponse, HttpServer, Responder,
+};
 use futures::FutureExt;
-use log::debug;
+use log::{debug, error};
 use rsbt_app::{
     request, App as RsbtApp, Command, CommandSender, RsbtResult, TokioMpscSender, TokioTypeFactory,
 };
 use rsbt_web_common::generated_web_common;
 use rsbt_web_wizard::generated_web_wizard;
+use tokio::sync::{oneshot, Mutex};
+
+#[get("/api/wizard/all-default")]
+async fn wizard_all_default(
+    quit_trigger: web::Data<Mutex<Option<oneshot::Sender<()>>>>,
+) -> impl Responder {
+    if let Some(quit_trigger) = quit_trigger.lock().await.take() {
+        if let Err(_) = quit_trigger.send(()) {
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+
+    HttpResponse::Ok()
+        .header("Content-Type", "text/html")
+        .body("<meta http-equiv=\"refresh\" content=\"5; url=/\" />")
+}
 
 #[post("/api/v1/action")]
 async fn api_v1_action(
@@ -55,13 +73,19 @@ async fn main() -> std::io::Result<()> {
     let need_initial_configuration = true;
 
     if need_initial_configuration {
-        HttpServer::new(move || {
+        let (quit_trigger_tx, quit_trigger_rx) = oneshot::channel();
+
+        let server_handler: web::Data<Mutex<Option<oneshot::Sender<()>>>> =
+            web::Data::new(Mutex::new(Some(quit_trigger_tx)));
+        let server = HttpServer::new(move || {
             let generated_web_common = generated_web_common();
             let generated_web_wizard = generated_web_wizard();
 
             debug!("Generated static assets for HTTP worker.");
 
             App::new()
+                .app_data(server_handler.clone())
+                .service(wizard_all_default)
                 .service(actix_web_static_files::ResourceFiles::new(
                     "/res",
                     generated_web_common,
@@ -72,8 +96,20 @@ async fn main() -> std::io::Result<()> {
                 ))
         })
         .bind("127.0.0.1:8080")?
-        .run()
-        .await?;
+        .run();
+
+        let server_clone = server.clone();
+        let quit_wizard_task = async move {
+            if let Err(err) = quit_trigger_rx.await {
+                error!("cannot trigger wizard quit: {}", err);
+                return;
+            }
+            server_clone.stop(true).await;
+        };
+
+        tokio::spawn(quit_wizard_task);
+
+        server.await?;
     }
 
     let app = RsbtApp::<TokioTypeFactory>::new(Default::default());
